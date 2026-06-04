e6a5d4faf97cc0981c56552ca38ce1d4c2dc6f94
Nester2432 <Kevin.maimbil.k@gmail.com>
2026-06-04 17:37:19 -0300

    font2

--- imagen.py
+++ imagen.py
@@ -16,8 +16,8 @@ X_CAJA = 195
 Y_CAJA = 410
 
 # Posición del número de confesión
-NUMERO_X = 320
-NUMERO_Y = 385
+NUMERO_X = 330
+NUMERO_Y = 390
 
 FUENTE_RUTA = os.path.join(
     os.path.dirname(__file__),
